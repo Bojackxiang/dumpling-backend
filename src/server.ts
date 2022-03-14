073a10a73787c078d1d069c4181bb956f601cec3
Alex <alex@q-83.com>
2022-03-15 00:30:09 +1100

    更新了 ts 的路径

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
+
 import * as express from "express";
 import { graphqlHTTP } from "express-graphql";
+import {Test} from "@models";
 import config from "./config";
-import { resolvers, schemas } from "./graphql";
+import { resolvers, schemas } from "./graphql-tool";
+
 
 export const start = () => {
     const app = express();
@@ -14,7 +17,9 @@ export const start = () => {
             graphiql: true,
         })
     );
-
+    
+    const test = new Test();
+    console.log(test)
     app.get("/test", (req, res) => {
         res.send("Hello World!");
     });
