21638444872e1db9622a4313d2ca8f0bb00d1e18
Alex <alex@q-83.com>
2022-04-02 15:31:42 +1100

    添加了 jwt 的 测试

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,5 @@ export default {
   BACKEND_PORT: process.env.BACKEND_PORT,
   MODE: process.env.MODE,
   MONGO_URI: 'mongodb+srv://dumpling-account:911004wsrA@cluster0.ee8ps.mongodb.net/dumpling?retryWrites=true&w=majority',
+  salt: "asd912980,.9,./.,"
 }
---
+++ src/utils/jwtUtils.ts
@@ -0,0 +1,31 @@
+import * as jwt from "jsonwebtoken";
+
+class JWTUtils {
+  static generateJWT<T>(data: T, secret: string, expiresIn: string = "24h") {
+    if (!secret || !data) {
+      return undefined;
+    }
+    return jwt.sign(
+      {
+        data,
+      },
+      secret,
+      {
+        expiresIn: "1s",
+      }
+    );
+  }
+
+  static verifyJWT(token: string, secret) {
+    try {
+      if (!token || !secret) {
+        return undefined;
+      }
+      return jwt.verify(token, secret);
+    } catch (error) {
+      throw new Error(error.message)
+    }
+  }
+}
+
+export default JWTUtils;
